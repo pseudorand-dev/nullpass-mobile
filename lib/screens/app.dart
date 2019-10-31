d0d5f072ad1326ede2459b1bafa8450cbbd867b8
Ilan Rasekh <ilan.rasekh@gmail.com>
2019-10-31 00:04:48 -0400

    update the app to replace the placeholder with the secret list screen as the primary screen of the app.

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -4,27 +4,86 @@
  */
 
 import 'package:flutter/material.dart';
-import 'package:nullpass/screens/placeholder.dart' as np;
+import 'package:nullpass/common.dart';
+import 'package:nullpass/screens/secretList.dart';
+import 'package:nullpass/secret.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class NullPassApp extends StatelessWidget{
+class NullPassApp extends StatefulWidget {
+  @override
+  _NullPassAppState createState() => _NullPassAppState();
+}
+
+class _NullPassAppState extends State<NullPassApp> {
+  List<Secret> _secrets = <Secret>[];
+  bool _loading = true;
+  bool debug = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    assert((debug = true) || true);
+    /*
+    assert(() {
+      debug = true;
+      // TODO: set logging level to debug
+      return true;
+    }());
+    */
+
+    if (debug) {
+      print('debug on');
+    }
+
+    if (sharedPrefs == null) {
+      SharedPreferences.getInstance().then((sp) {
+        sharedPrefs = sp;
+        setupSharedPreferences();
+      });
+    }
+
+    NullPassDB helper = NullPassDB.instance;
+
+    helper.getAllSecrets().then((result) {
+      setState(() {
+        _secrets = result;
+        _loading = false;
+      });
+    });
+  }
+
+  void _reloadSecretList(result) async {
+    if (isTrue(result)) {
+      NullPassDB npDB = NullPassDB.instance;
+      List<Secret> sList = await npDB.getAllSecrets();
+      setState(() {
+        _secrets = sList;
+      });
+    }
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NullPass',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: np.Placeholder('NullPass'),
-    );
+        title: 'NullPass',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: SecretList(
+          loading: _loading,
+          items: _secrets,
+          reloadSecretList: _reloadSecretList,
+        ));
   }
 }
