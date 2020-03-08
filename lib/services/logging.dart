ae959a4276add19d036fc966d9f628a0807c1e08
Ilan Rasekh <ilan.rasekh@gmail.com>
2020-03-08 10:45:42 -0400

    setup basic 'logging' service to print debug logs in a standard way

---
+++ lib/services/logging.dart
@@ -0,0 +1,21 @@
+import 'dart:developer' as developer;
+
+// Is the App in Debug Mode
+bool isDebug = false;
+
+// The package name to log under
+const String _logPackage = "dev.pseudorand.nullpass";
+
+enum LogLevel { everything, verbose, debug, info, warn, error, fatal, panic }
+
+class Log {
+  static LogLevel logLevel;
+
+  static void debug(dynamic message,
+      {String source, Object error, StackTrace stackTrace, int severity: 0}) {
+    if (isDebug) {
+      developer.log(message.toString(),
+          name: _logPackage, time: DateTime.now());
+    }
+  }
+}
