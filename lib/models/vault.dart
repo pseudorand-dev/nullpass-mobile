fb0ff66d0a7865aa32f2d1b34256ff7ccd50486d
Ilan Rasekh <ilan.rasekh@gmail.com>
2020-03-26 14:06:33 -0400

    update the vault model to support the datastore service and be able to store in and fetch from db

--- lib/models/vault.dart
+++ lib/models/vault.dart
@@ -3,9 +3,9 @@
  * Copyright (c) 2020 Pseudorand Development. All rights reserved.
  */
 
-// TODO: replace with better terminology
 import 'package:uuid/uuid.dart';
 
+// TODO: replace with better terminology
 enum VaultSource { Internal, External }
 
 String vaultSourceToString(VaultSource input) {
@@ -17,18 +17,43 @@ VaultSource vaultSourceFromString(String input) {
       (vs) => vaultSourceToString(vs).toLowerCase() == input.toLowerCase());
 }
 
-const String _UID_KEY = "uid";
-const String _NICKNAME_KEY = "nickname";
-const String _VAULTSOURCE_KEY = "source";
-const String _VAULTSOURCEID_KEY = "source_id";
+const String _VAULT_UID_KEY = "_id";
+const String _VAULT_NICKNAME_KEY = "nickname";
+const String _VAULT_SOURCE_KEY = "source";
+const String _VAULT_SOURCEID_KEY = "source_id";
+const String _VAULT_ISDEFAULT_KEY = "is_default";
+const String _VAULT_SORTKEY_KEY = "sort_key";
+const String _VAULT_CREATED_KEY = "created_at";
+const String _VAULT_MODIFIED_KEY = "modified_at";
+
+final String vaultTableName = 'vaults';
+final String columnVaultId = _VAULT_UID_KEY;
+final String columnVaultNickname = _VAULT_NICKNAME_KEY;
+final String columnVaultSource = _VAULT_SOURCE_KEY;
+final String columnVaultSourceId = _VAULT_SOURCEID_KEY;
+final String columnVaultIsDefault = _VAULT_ISDEFAULT_KEY;
+final String columnVaultSortKey = _VAULT_SORTKEY_KEY;
+final String columnVaultCreated = _VAULT_CREATED_KEY;
+final String columnVaultModified = _VAULT_MODIFIED_KEY;
 
 class Vault {
   String uid;
   String nickname;
   VaultSource source;
   String sourceId;
+  bool isDefault;
+  DateTime createdAt;
+  DateTime modifiedAt;
+  get sortKey => nickname.trim().toLowerCase() ?? "";
 
-  Vault({uid, this.nickname, this.source, this.sourceId})
+  Vault(
+      {uid,
+      this.nickname,
+      this.source,
+      this.sourceId,
+      this.isDefault = false,
+      this.createdAt,
+      this.modifiedAt})
       : this.uid = _populateUID(uid);
 
   static String _populateUID(String uid) {
@@ -41,23 +66,34 @@ class Vault {
 
   @override
   String toString() {
-    return "{\"$_UID_KEY\":\"${this.uid}\"," +
-        "\"$_NICKNAME_KEY\":\"${this.nickname}\"," +
-        "\"$_VAULTSOURCE_KEY\":\"${this.source}\"," +
-        "\"$_VAULTSOURCEID_KEY\":\"${this.sourceId}\"}";
+    return "{\"$_VAULT_UID_KEY\":\"${this.uid}\"," +
+        "\"$_VAULT_NICKNAME_KEY\":\"${this.nickname}\"," +
+        "\"$_VAULT_SOURCE_KEY\":\"${this.source}\"," +
+        "\"$_VAULT_SOURCEID_KEY\":\"${this.sourceId}\"" +
+        "\"$_VAULT_ISDEFAULT_KEY\":\"${this.isDefault}\"" +
+        "\"$_VAULT_CREATED_KEY\":\"${this.createdAt}\"" +
+        "\"$_VAULT_MODIFIED_KEY\":\"${this.modifiedAt}\"}";
   }
 
-  Map<String, dynamic> toJson() => {
-        _UID_KEY: this.uid,
-        _NICKNAME_KEY: this.nickname,
-        _VAULTSOURCE_KEY: this.source,
-        _VAULTSOURCEID_KEY: this.sourceId,
+  Map<String, dynamic> toJson() => this.toMap();
+  Map<String, dynamic> toMap() => {
+        _VAULT_UID_KEY: this.uid,
+        _VAULT_NICKNAME_KEY: this.nickname,
+        _VAULT_SOURCE_KEY: vaultSourceToString(this.source),
+        _VAULT_SOURCEID_KEY: this.sourceId,
+        _VAULT_ISDEFAULT_KEY: this.isDefault,
+        _VAULT_SORTKEY_KEY: this.sortKey,
+        _VAULT_CREATED_KEY: this.createdAt.toIso8601String(),
+        _VAULT_MODIFIED_KEY: this.modifiedAt.toIso8601String(),
       };
 
   Vault.fromMap(Map input) {
-    this.uid = input[_UID_KEY];
-    this.nickname = input[_NICKNAME_KEY];
-    this.source = input[_VAULTSOURCE_KEY];
-    this.sourceId = input[_VAULTSOURCEID_KEY];
+    this.uid = input[_VAULT_UID_KEY];
+    this.nickname = input[_VAULT_NICKNAME_KEY];
+    this.source = vaultSourceFromString(input[_VAULT_SOURCE_KEY]);
+    this.sourceId = input[_VAULT_SOURCEID_KEY];
+    this.isDefault = input[_VAULT_ISDEFAULT_KEY] ?? false;
+    this.createdAt = DateTime.tryParse(input[_VAULT_CREATED_KEY]);
+    this.modifiedAt = DateTime.tryParse(input[_VAULT_MODIFIED_KEY]);
   }
 }
