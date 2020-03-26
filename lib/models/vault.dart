5b86dd19cafb0b8d3a11cf4c07b93f29be3a4a07
Ilan Rasekh <ilan.rasekh@gmail.com>
2020-03-26 00:41:31 -0400

    update the vault model to represent a vault that secrets are tied to, this will be needed to represent vault data in the datastore for many reasons such as displaying vaults in the appdrawer or as a set for selection when creating new secrets

--- lib/models/vault.dart
+++ lib/models/vault.dart
@@ -4,12 +4,60 @@
  */
 
 // TODO: replace with better terminology
+import 'package:uuid/uuid.dart';
+
 enum VaultSource { Internal, External }
 
+String vaultSourceToString(VaultSource input) {
+  return input.toString().substring(input.toString().indexOf(".") + 1);
+}
+
+VaultSource vaultSourceFromString(String input) {
+  return VaultSource.values.firstWhere(
+      (vs) => vaultSourceToString(vs).toLowerCase() == input.toLowerCase());
+}
+
+const String _UID_KEY = "uid";
+const String _NICKNAME_KEY = "nickname";
+const String _VAULTSOURCE_KEY = "source";
+const String _VAULTSOURCEID_KEY = "source_id";
+
 class Vault {
-  String id;
+  String uid;
   String nickname;
   VaultSource source;
+  String sourceId;
 
-  Vault(this.id, this.nickname, this.source);
+  Vault({uid, this.nickname, this.source, this.sourceId})
+      : this.uid = _populateUID(uid);
+
+  static String _populateUID(String uid) {
+    if (uid == null || uid.trim().isEmpty) {
+      return Uuid().v4();
+    } else {
+      return uid;
+    }
+  }
+
+  @override
+  String toString() {
+    return "{\"$_UID_KEY\":\"${this.uid}\"," +
+        "\"$_NICKNAME_KEY\":\"${this.nickname}\"," +
+        "\"$_VAULTSOURCE_KEY\":\"${this.source}\"," +
+        "\"$_VAULTSOURCEID_KEY\":\"${this.sourceId}\"}";
+  }
+
+  Map<String, dynamic> toJson() => {
+        _UID_KEY: this.uid,
+        _NICKNAME_KEY: this.nickname,
+        _VAULTSOURCE_KEY: this.source,
+        _VAULTSOURCEID_KEY: this.sourceId,
+      };
+
+  Vault.fromMap(Map input) {
+    this.uid = input[_UID_KEY];
+    this.nickname = input[_NICKNAME_KEY];
+    this.source = input[_VAULTSOURCE_KEY];
+    this.sourceId = input[_VAULTSOURCEID_KEY];
+  }
 }
