7edcb6253b1f910a31391d407a76f2c2050af11a
Ilan Rasekh <ilan.rasekh@gmail.com>
2020-12-13 15:53:54 -0500

    [UPDATE] - the Main activity to maintain support for the Secure Flags in android and turning on android embedding 2

--- android/app/src/main/kotlin/dev/pseudorand/nullpass/MainActivity.kt
+++ android/app/src/main/kotlin/dev/pseudorand/nullpass/MainActivity.kt
@@ -5,16 +5,33 @@
 
 package dev.pseudorand.nullpass
 
+import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager.LayoutParams
-
-import io.flutter.app.FlutterActivity
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+  //
+  /*
+  @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    getWindow().addFlags(LayoutParams.FLAG_SECURE)
     GeneratedPluginRegistrant.registerWith(this)
+    flutterView.setSecure(true)
+    this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
   }
+  // */
+  // /*
+   @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+    flutterView.setSecure(true)
+    this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+  }
+  // */
 }
