29b6845757c03a4653c8db88231bdb24fad93a6e
Ilan Rasekh <ilan.rasekh@gmail.com>
2021-01-13 14:39:51 -0500

    remove no longer necessary native code that was used for hiding the screen when it moves to the background (like when going to the app switcher)

--- android/app/src/main/kotlin/dev/pseudorand/nullpass/MainActivity.kt
+++ android/app/src/main/kotlin/dev/pseudorand/nullpass/MainActivity.kt
@@ -5,33 +5,6 @@
 
 package dev.pseudorand.nullpass
 
-import android.os.Build
-import android.os.Bundle
-import android.view.WindowManager
-import androidx.annotation.RequiresApi
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  //
-  /*
-  @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-    flutterView.setSecure(true)
-    this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
-  }
-  // */
-  // /*
-   @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-    super.configureFlutterEngine(flutterEngine)
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-    flutterView.setSecure(true)
-    this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
-  }
-  // */
-}
+class MainActivity: FlutterActivity() { }
